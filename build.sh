6f5149bd15cbafdd6a33c00716585bf989c39032
Alex Kubacki <alex@aptible.com>
2018-04-30 16:32:25 -0400

    Update nginx version

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+
 #update and install dependencies
 apt-get update
 apt-get install -y git wget build-essential libpcre3 libpcre3-dev libssl-dev libtool autoconf apache2-dev libxml2-dev libcurl4-openssl-dev
@@ -13,9 +15,9 @@ make
 
 #make nginx
 cd /
-wget http://nginx.org/download/nginx-1.10.2.tar.gz
-tar xvzf nginx-1.10.2.tar.gz
-cd ../nginx-1.10.2
+wget http://nginx.org/download/nginx-1.14.0.tar.gz
+tar xvzf nginx-1.14.0.tar.gz
+cd ../nginx-1.14.0
 ./configure --user=root --group=root --with-debug --with-ipv6 --with-http_ssl_module --add-module=/usr/src/modsecurity/nginx/modsecurity --with-http_ssl_module --without-http_access_module --without-http_auth_basic_module --without-http_autoindex_module --without-http_empty_gif_module --without-http_fastcgi_module --without-http_referer_module --without-http_memcached_module --without-http_scgi_module --without-http_split_clients_module --without-http_ssi_module --without-http_uwsgi_module
 make
 make install
@@ -32,7 +34,7 @@ mv /usr/local/nginx/conf/rules/REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf{.exam
 mv /usr/local/nginx/conf/rules/RESPONSE-999-EXCLUSION-RULES-AFTER-CRS.conf{.example,}
 
 apt-get purge -y build-essential wget git
-rm /nginx-1.10.2.tar.gz
+rm /nginx-1.14.0.tar.gz
 
 
 
