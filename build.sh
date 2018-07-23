7cbbf19f31ba9325ebd1b9c08a34c63a5769e1f5
tonyzeng91 <tonyzeng1991@gmail.com>
2018-07-23 10:53:42 -0400

    update modsecurity version to v3.0

--- build.sh
+++ build.sh
@@ -7,24 +7,30 @@ apt-get install -y git wget build-essential libpcre3 libpcre3-dev libssl-dev lib
 
 #make modsecurity
 cd /usr/src/
-git clone https://github.com/SpiderLabs/ModSecurity.git /usr/src/modsecurity
+git clone --depth 1 -b v3/master --single-branch https://github.com/SpiderLabs/ModSecurity.git /usr/src/modsecurity
 cd /usr/src/modsecurity
-./autogen.sh
-./configure --enable-standalone-module --disable-mlogc
+git submodule init
+git submodule update
+./build.sh
+./configure
 make
+make install
+
+#libmodsecurity with nginx
+cd /root 
+git clone --depth 1 https://github.com/SpiderLabs/ModSecurity-nginx.git
 
 #make nginx
 cd /
 wget http://nginx.org/download/nginx-1.14.0.tar.gz
 tar xvzf nginx-1.14.0.tar.gz
-cd ../nginx-1.14.0
-./configure --user=root --group=root --with-debug --with-ipv6 --with-http_ssl_module --add-module=/usr/src/modsecurity/nginx/modsecurity --with-http_ssl_module --without-http_access_module --without-http_auth_basic_module --without-http_autoindex_module --without-http_empty_gif_module --without-http_fastcgi_module --without-http_referer_module --without-http_memcached_module --without-http_scgi_module --without-http_split_clients_module --without-http_ssi_module --without-http_uwsgi_module
+cd nginx-1.14.0
+./configure --user=root --group=root --with-debug --with-ipv6 --with-http_ssl_module --add-module=/root/ModSecurity-nginx --with-http_ssl_module --without-http_access_module --without-http_auth_basic_module --without-http_autoindex_module --without-http_empty_gif_module --without-http_fastcgi_module --without-http_referer_module --without-http_memcached_module --without-http_scgi_module --without-http_split_clients_module --without-http_ssi_module --without-http_uwsgi_module
 make
 make install
 
 #configure env
 ln -s /usr/local/nginx/sbin/nginx /bin/nginx
-cp /usr/src/modsecurity/unicode.mapping /usr/local/nginx/conf/
 mkdir -p /opt/modsecurity/var/audit/
 
 #install signature
@@ -35,6 +41,3 @@ mv /usr/local/nginx/conf/rules/RESPONSE-999-EXCLUSION-RULES-AFTER-CRS.conf{.exam
 
 apt-get purge -y build-essential wget git
 rm /nginx-1.14.0.tar.gz
-
-
-
